344fa2997df00b866609844ebf1bc9ff39661f08
anhnq193 <cafedang1993@gmail.com>
2018-06-01 03:10:29 +0700

    first commit

---
+++ database/migrations/2018_05_31_191019_create_apartments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateApartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('address');
+            $table->bigInteger('price');
+            $table->string('short_info');
+            $table->text('detail_info');
+            $table->string('img_url');
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartments');
+    }
+}
---
+++ database/seeds/ApartmentTableSeeder.php
@@ -0,0 +1,198 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ApartmentTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        \Illuminate\Support\Facades\DB::table('apartments')->insert([
+            [
+                'name'=> 'home '.random_int(1, 100),
+                'address'=>random_int(1,100) . str_random(10),
+                'price'=> random_int(1000, 20000),
+                'short_info'=>str_random(22),
+                'detail_info'=>str_random(50),
+                'img_url'=>str_random(20),
+                'status'=>random_int(0,1)
+            ],
+            [
+                'name'=> 'home '.random_int(1, 100),
+                'address'=>random_int(1,100) . str_random(10),
+                'price'=> random_int(1000, 20000),
+                'short_info'=>str_random(22),
+                'detail_info'=>str_random(50),
+                'img_url'=>str_random(20),
+                'status'=>random_int(0,1)
+            ],
+            [
+                'name'=> 'home '.random_int(1, 100),
+                'address'=>random_int(1,100) . str_random(10),
+                'price'=> random_int(1000, 20000),
+                'short_info'=>str_random(22),
+                'detail_info'=>str_random(50),
+                'img_url'=>str_random(20),
+                'status'=>random_int(0,1)
+            ],
+            [
+                'name'=> 'home '.random_int(1, 100),
+                'address'=>random_int(1,100) . str_random(10),
+                'price'=> random_int(1000, 20000),
+                'short_info'=>str_random(22),
+                'detail_info'=>str_random(50),
+                'img_url'=>str_random(20),
+                'status'=>random_int(0,1)
+            ],
+            [
+                'name'=> 'home '.random_int(1, 100),
+                'address'=>random_int(1,100) . str_random(10),
+                'price'=> random_int(1000, 20000),
+                'short_info'=>str_random(22),
+                'detail_info'=>str_random(50),
+                'img_url'=>str_random(20),
+                'status'=>random_int(0,1)
+            ],
+            [
+                'name'=> 'home '.random_int(1, 100),
+                'address'=>random_int(1,100) . str_random(10),
+                'price'=> random_int(1000, 20000),
+                'short_info'=>str_random(22),
+                'detail_info'=>str_random(50),
+                'img_url'=>str_random(20),
+                'status'=>random_int(0,1)
+            ],
+            [
+                'name'=> 'home '.random_int(1, 100),
+                'address'=>random_int(1,100) . str_random(10),
+                'price'=> random_int(1000, 20000),
+                'short_info'=>str_random(22),
+                'detail_info'=>str_random(50),
+                'img_url'=>str_random(20),
+                'status'=>random_int(0,1)
+            ],
+            [
+                'name'=> 'home '.random_int(1, 100),
+                'address'=>random_int(1,100) . str_random(10),
+                'price'=> random_int(1000, 20000),
+                'short_info'=>str_random(22),
+                'detail_info'=>str_random(50),
+                'img_url'=>str_random(20),
+                'status'=>random_int(0,1)
+            ],
+            [
+                'name'=> 'home '.random_int(1, 100),
+                'address'=>random_int(1,100) . str_random(10),
+                'price'=> random_int(1000, 20000),
+                'short_info'=>str_random(22),
+                'detail_info'=>str_random(50),
+                'img_url'=>str_random(20),
+                'status'=>random_int(0,1)
+            ],
+            [
+                'name'=> 'home '.random_int(1, 100),
+                'address'=>random_int(1,100) . str_random(10),
+                'price'=> random_int(1000, 20000),
+                'short_info'=>str_random(22),
+                'detail_info'=>str_random(50),
+                'img_url'=>str_random(20),
+                'status'=>random_int(0,1)
+            ],
+            [
+                'name'=> 'home '.random_int(1, 100),
+                'address'=>random_int(1,100) . str_random(10),
+                'price'=> random_int(1000, 20000),
+                'short_info'=>str_random(22),
+                'detail_info'=>str_random(50),
+                'img_url'=>str_random(20),
+                'status'=>random_int(0,1)
+            ],
+            [
+                'name'=> 'home '.random_int(1, 100),
+                'address'=>random_int(1,100) . str_random(10),
+                'price'=> random_int(1000, 20000),
+                'short_info'=>str_random(22),
+                'detail_info'=>str_random(50),
+                'img_url'=>str_random(20),
+                'status'=>random_int(0,1)
+            ],
+            [
+                'name'=> 'home '.random_int(1, 100),
+                'address'=>random_int(1,100) . str_random(10),
+                'price'=> random_int(1000, 20000),
+                'short_info'=>str_random(22),
+                'detail_info'=>str_random(50),
+                'img_url'=>str_random(20),
+                'status'=>random_int(0,1)
+            ],
+            [
+                'name'=> 'home '.random_int(1, 100),
+                'address'=>random_int(1,100) . str_random(10),
+                'price'=> random_int(1000, 20000),
+                'short_info'=>str_random(22),
+                'detail_info'=>str_random(50),
+                'img_url'=>str_random(20),
+                'status'=>random_int(0,1)
+            ],
+            [
+                'name'=> 'home '.random_int(1, 100),
+                'address'=>random_int(1,100) . str_random(10),
+                'price'=> random_int(1000, 20000),
+                'short_info'=>str_random(22),
+                'detail_info'=>str_random(50),
+                'img_url'=>str_random(20),
+                'status'=>random_int(0,1)
+            ],
+            [
+                'name'=> 'home '.random_int(1, 100),
+                'address'=>random_int(1,100) . str_random(10),
+                'price'=> random_int(1000, 20000),
+                'short_info'=>str_random(22),
+                'detail_info'=>str_random(50),
+                'img_url'=>str_random(20),
+                'status'=>random_int(0,1)
+            ],
+            [
+                'name'=> 'home '.random_int(1, 100),
+                'address'=>random_int(1,100) . str_random(10),
+                'price'=> random_int(1000, 20000),
+                'short_info'=>str_random(22),
+                'detail_info'=>str_random(50),
+                'img_url'=>str_random(20),
+                'status'=>random_int(0,1)
+            ],
+            [
+                'name'=> 'home '.random_int(1, 100),
+                'address'=>random_int(1,100) . str_random(10),
+                'price'=> random_int(1000, 20000),
+                'short_info'=>str_random(22),
+                'detail_info'=>str_random(50),
+                'img_url'=>str_random(20),
+                'status'=>random_int(0,1)
+            ],
+            [
+                'name'=> 'home '.random_int(1, 100),
+                'address'=>random_int(1,100) . str_random(10),
+                'price'=> random_int(1000, 20000),
+                'short_info'=>str_random(22),
+                'detail_info'=>str_random(50),
+                'img_url'=>str_random(20),
+                'status'=>random_int(0,1)
+            ],
+            [
+                'name'=> 'home '.random_int(1, 100),
+                'address'=>random_int(1,100) . str_random(10),
+                'price'=> random_int(1000, 20000),
+                'short_info'=>str_random(22),
+                'detail_info'=>str_random(50),
+                'img_url'=>str_random(20),
+                'status'=>random_int(0,1)
+            ]
+        ]);
+    }
+}
